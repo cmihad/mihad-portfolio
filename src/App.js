86e4590993f882c7c3f897d044683c87a8a5530d
cmihad@gmail.com <cmihad@gmail.com>
2020-08-04 00:48:33 +0600

    first commit to the porfolio site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ParticleComponent from  './components/ParticleComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <ParticleComponent />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1>Testing</h1>
+          {/* You can render <Route> and <NavTabs /> here */}
+         
+         
+          </div>
+            <div class="flex h-screen">
+              <div class="m-auto">
+              <h1 className=" text-6xl" >🅼🅸🅷🅰🅳</h1>
+            
+              </div>
+          </div>
+          </div>
+      
+          </Router>
   );
 }
-
 export default App;
---
+++ src/components/ParticleComponent.js
@@ -0,0 +1,128 @@
+import React from "react";
+import Particles from "react-particles-js";
+
+export default () => (
+  <div
+    style={{
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%"
+      
+    }}
+  >
+    <Particles
+      params={{
+        particles: {
+          number: {
+            value: 150,
+            density: {
+              enable: true,
+              value_area: 2000
+            }
+          },
+          color: {
+            value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"]
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#fff"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 1,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false
+            }
+          },
+          size: {
+            value: 8,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 10,
+              size_min: 10,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#808080",
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 5,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "window",
+          events: {
+            onhover: {
+              enable: true,
+              mode: "grab"
+            },
+            onclick: {
+              enable: true,
+              mode: "push"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 140,
+              line_linked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        retina_detect: true
+      }}
+    />
+  </div>
+);
